0a80e168dddc9d3488078492c88ade519f0dc87c
aryaMode <adhrangdhria8@gmail.com>
2019-02-13 21:01:08 +0530

    Second Commit

--- src/Q2.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.util.*;
-public class Q2 {
-    public static void main(String[]args){
-        Scanner in  = new Scanner(System.in);
-        int n = Integer.parseInt(JOptionPane.showInputDialog("What the number of elements in the group? "));
-        String arya[] = new String[n];
-    }
-}
